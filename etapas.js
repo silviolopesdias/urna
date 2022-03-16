c75c5e366454acd3109045d90712cef34c6a2758
silviolopesdias <silvio.sandra@live.com>
2022-03-16 14:12:22 -0300

    my first commit

---
+++ etapas.js
@@ -0,0 +1,45 @@
+let etapas = [
+
+{
+    titulo: "Vereador",
+    numeros: 5,
+    candidatos: [
+        {
+            numero: "01234",
+            nome: "fulano de Tal",
+            partido: "ABC",
+            fotos: [{url: "09876.jpg", legenda: "Vereador"}]
+        },
+        {
+            numero: "12345",
+            nome: "Beltrano da Silva",
+            partido: "PPP",
+            fotos: [{url: "25273.jpg", legenda: "Vereador"}]
+        },
+    ]
+},
+
+{
+    titulo: "Prefeito",
+    numeros: 2,
+    candidatos: [
+        {
+            numero: "12",
+            nome: "Beltrano de Tal",
+            vice: "beltrano",
+            partido: "ABC",
+            fotos: [{url: "12.jpeg", legenda: "Prefeito"},
+                    {url: "16.jpeg", legenda: "Vice-Prefeito", small: true }]
+        },
+        {
+            numero: "34",
+            nome: "fulano de Silva",
+            vice: "fulano",
+            partido: "PPP",
+            fotos: [{url: "19.jpg", legenda: "Prefeito"},
+                    {url: "16.jpeg", legenda: "Vice-Prefeito", small: true}]
+        },
+    ]
+}
+
+]
